4c4e5a24d34c45d7ca7a13f49c749041cb3ee0cb
Luckytime1996 <larb26656@gmail.com>
2026-03-21 10:07:30 +0700

    feat: use react tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { TooltipProvider } from '@/components/ui/tooltip'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <TooltipProvider>
-      <App />
-    </TooltipProvider>
-  </StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+import { TooltipProvider } from "./components/ui/tooltip";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <TooltipProvider>
+        {" "}
+        <RouterProvider router={router} />
+      </TooltipProvider>
+    </StrictMode>,
+  );
+}
